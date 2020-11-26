67337370d7c863b995f43d6ce9d17cefdbc6d939
SujithAcharya99 <52646806+SujithAcharya99@users.noreply.github.com>
2020-11-26 18:12:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,132 +1,3 @@
-// const app = require('./app');
-//******************************************** */
-// const express = require('express');
-// require('./db/mongoose');
-// // const User = require('./models/user');
-// // const Task = require('./models/task');
-// const userRouter = require('./routers/user');
-// const taskRouter = require('./routers/task');
-//************************************************** */
-// const app = express();
-
-
-// const port = process.env.PORT;
-// const port = 3000;
-
-
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'images',
-//     limits: {
-//         fileSize: 1000000
-//     },
-//     fileFilter(req, file, cb) {
-
-//         // if (!file.originalname.endsWith('.pdf')) {
-//         if (!file.originalname.match(/\.(doc|docx)$/)){
-//             return cb(new Error('File must be a Word Document'));
-//         }
-
-//         cb(undefined, true);
-        // cb(new Error('File must be a PDF'));
-        // cb(undefined, true);
-        // cb(undefined, false);
-
-//     }
-// });
-
-// const errrorMiddleware = (req, res, next) => {
-//     throw new Error('from my middleware');
-// }
-
-//upload.single('upload')
-
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send();
-// }, (error, req, res, next) => {
-//     res.status(400).send({ error: error.message });
-// });
-
-
-
-
-// app.use((req, res, next) => {
-//     // console.log(req.method, req.path);
-//     // next();
-//     if (req.method === 'GET') {
-//         res.send('GET request are disabled')
-//     } else {
-//         next();
-//     }
-
-// })
-
-//***********maintenance mode*********** */
-
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon..!');
-// });
-
-//*****************************************8 */
-// app.use(express.json());
-// app.use(userRouter);
-// app.use(taskRouter);
-
-// app.listen(port, () => {
-//     console.log('server is up on port:' + port);
-// });
-
-//const bcrypt = require('bcryptjs');
-
-// const jwt = require('jsonwebtoken');
-
-// const myFunction = async () => {
-
-//     const token = jwt.sign({ _id: 'add123' }, 'thisismynewcourse', { expiresIn: '3 second'});
-//     console.log(token);
-
-//     const data = jwt.verify(token, 'thisismynewcourse');
-//     console.log(data);
-    // const password = 'Red12345!';
-    // const hashedPassword = await bcrypt.hash(password, 8);
-
-    // console.log(password);
-    // console.log(hashedPassword);
-
-    // const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
-
-    // console.log(isMatch);
-//}
-
-// myFunction();
-
-// const pet = {
-//     name: 'munna'
-// }
-
-// pet.toJSON = function () {
-//     // console.log(this);
-//     // return this;
-//     return {}
-// }
-// console.log(JSON.stringify(pet));
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-    // const task = await Task.findById('5fb4b50f04ea241ab42c5866');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    // const user = await User.findById('5fb4b40f7814cc1a420996db');
-    // await user.populate('tasks').execPopulate();
-    // console.log(user.tasks);
-// }
-
-
-// main();
-
 const express = require('express');
 require('./db/mongoose');
 const Admin = require('./models/admin');
@@ -142,10 +13,6 @@ const bodyparser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// console.log(__dirname);
-// console.log(path.join(__dirname, '../public'));
-
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname,'../views/views');
@@ -183,15 +50,6 @@ app.get('/test', async (req, res) => {
  
 })
 
-// app.get('/test2', async (req, res) => {
-//     res.render('test2',{
-//         title : 'Login Page',
-//         body : 'Sujith S'
-//     });
- 
-// })
-
-
 
 app.get('/about', (req, res) => {
     res.render('about',{
@@ -199,14 +57,6 @@ app.get('/about', (req, res) => {
         name : 'Sujith S'
     });
 })
-
-// app.post('/test', async (req, res) => {
-
-//     const email = await req.body;
-
-//     console.log(email);
-//   res.send('success')
-// })
 
 //************************Login*****************88*8 */
 app.get('/login', (req, res) => {
@@ -216,30 +66,6 @@ app.get('/login', (req, res) => {
 
 
 app.post('/login', async (req, res) => {
-    // try {
-    //     // const email = req.body.email;
-    //     // console.log(email)
-    //     const admin = await Admin.findOne({email: req.body.email, password: req.body.password});
-    //     if (admin) {
-           
-    //         return res.send('welcome admin');
-    //     }
-        
-    //     const user = await User.findOne({email: req.body.email, password: req.body.password});
-    //     // const token = await user.generateAuToken();
-    //     if (!user) {
-    //         return  res.send({
-    //             error : 'worng credentials'
-    //         })
-    //     } else {
-    //         // res.send(user);
-    //         res.redirect('/users');
-
-    //     }
-        
-    // } catch (e) {
-    //     res.status(400).send();
-    // }
     try {
 
         const admin = await Admin.findOne({email: req.body.email, password: req.body.password});
@@ -260,20 +86,14 @@ app.post('/login', async (req, res) => {
              });
          }
          console.log(user)
-        //  res.send({ user, token });
-    //     Us.find({}, {}, function(e, docs) {
 
-    //         res.render('user-list', {'userlist' : docs});
-
-    //   });
         res.render('test2',{
             title : 'student Page',
             // body : user.name,
             user
             
         });
-        //res.send(user.roll);
-        // res.redirect('/user')
+
         }
     } catch (e) {
         res.status(400).send();
@@ -321,18 +141,6 @@ app.post('/users/logoutAll', auth, async (req, res) => {
     }
 });
 
-//************************user not assigned Page********* */
-
-// app.get('/user', (req, res) => {
-//     // const name = new User.findOne({email: req.body.email})
-//     // console.log(res.send(req.user));
-//     res.render('/notAssigned',{
-//         title : 'Home Page',
-//         user: 'user',
-//         message : 'Pls wait for Admin to assigned'
-//      });
-
-// })
 
 
 
@@ -479,9 +287,6 @@ app.get('/user/edit/:id', async (req, res) => {
     })
 })
 
-// router.patch('/users/:id', async (req, res) => {
-
-    // router.patch('/users/me', auth, async (req, res) => {
 app.post('/user/edit/:id', async (req, res) => {
         // console.log(req.body);
         const _id = req.params.id;
@@ -500,9 +305,7 @@ app.post('/user/edit/:id', async (req, res) => {
         try {
         //    const user = await User.findOneAndUpdate(email, req.body, { new : true, runValidators: true});
          const user = await User.findOneAndUpdate({ _id }, req.body, { new: true })
-        //    const user = await User.findById(_id);
-        // console.log(user)
-           
+             
            updates.forEach((update) => user[update] = req.body[update])
     
         //    await req.user.save();
@@ -518,14 +321,6 @@ app.post('/user/edit/:id', async (req, res) => {
         }
     })
 
-    // app.get('/users', (req, res) => {   
-//     User.find({}).then((user) => {
-//         res.send(user);
-//     }).catch((e) => {
-//         res.status(500).send(e);
-//     })
-// });
-
 //***********************HELP******************** */
 app.get('/help', (req, res) =>{
     res.render('help',{
@@ -536,8 +331,6 @@ app.get('/help', (req, res) =>{
 });
 //*************************error Page************** */
 
-
-
 app.get('*', (req, res) =>{
     // res.send('My 404 ERROR PAGE');
     res.render('404',{
@@ -546,11 +339,6 @@ app.get('*', (req, res) =>{
      errorMessage : 'Page Not Found'
  });
 });
-
-
-
-
-
 
 app.listen(port, () => {
     console.log('server is up on port:' + port);
